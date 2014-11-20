711b8c58b4ec62c071dcf3fed967484ab6866907
ThomasDuin <t.d.duinkerken@gmail.com>
2014-11-20 09:55:11 +0100

    Map toegevoegd

--- MovinIndoor/app/src/main/java/project/movinindoor/MapsActivity.java
+++ MovinIndoor/app/src/main/java/project/movinindoor/MapsActivity.java
@@ -10,11 +10,55 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.android.gms.maps.model.TileProvider;
+import com.google.android.gms.maps.model.UrlTileProvider;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     public final String TAG = "MapsActivity";
+
+    TileProvider tileProvider = new UrlTileProvider(256, 256) {
+        @Override
+        public URL getTileUrl(int x, int y, int zoom) {
+
+    /* Define the URL pattern for the tile images */
+            String s = String.format("https://c6cad6a5034e906cdc0cb7705f1e8d6f00b847f2.googledrive.com/host/0B_NvHdXrVsSwTXZ0dmxEejlOeHc/%d/%d/%d.png",
+                    zoom, x, y);
+
+            if (!checkTileExists(x, y, zoom)) {
+                return null;
+            }
+
+            try {
+                return new URL(s);
+            } catch (MalformedURLException e) {
+                throw new AssertionError(e);
+            }
+        }
+
+        /*
+         * Check that the tile server supports the requested x, y and zoom.
+         * Complete this stub according to the tile range you support.
+         * If you support a limited range of tiles at different zoom levels, then you
+         * need to define the supported x, y range at each zoom level.
+         */
+        private boolean checkTileExists(int x, int y, int zoom) {
+            int minZoom = 12;
+            int maxZoom = 16;
+
+            if ((zoom < minZoom || zoom > maxZoom)) {
+                return false;
+            }
+
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,5 +123,8 @@ public class MapsActivity extends FragmentActivity {
      */
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(52.49951, 6.07869)).title("Marker"));
+
+        TileOverlay tileOverlay = mMap.addTileOverlay(new TileOverlayOptions()
+                .tileProvider(tileProvider));
     }
 }
