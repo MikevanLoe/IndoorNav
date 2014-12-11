5274839c5341d3f156f01f616bf690691bb236c0
DaveyHeeren <davey@daveyheeren.nl>
2014-12-11 16:26:47 +0100

    Route Building to Building

--- MovinIndoor/app/src/main/java/project/movinindoor/Readers/RepairReader.java
+++ MovinIndoor/app/src/main/java/project/movinindoor/Readers/RepairReader.java
@@ -65,7 +65,7 @@ public class RepairReader {
                     int floor1 = Integer.valueOf(splitFloor[0].substring(1));
                     String location = splitFloor[1];
                     LatLng latLng1 = new LatLng(Double.valueOf(clat), Double.valueOf(clong));
-                    Reparation.StatusEnum statusEnum;
+                    Reparation.StatusEnum statusEnum = Reparation.StatusEnum.NEW;
 
                     try {
                         buildingEnum = Reparation.BuildingEnum.valueOf(building);
@@ -74,11 +74,22 @@ public class RepairReader {
                         buildingEnum = Reparation.BuildingEnum.A;
                     }
 
-                    try {
-                        statusEnum = Reparation.StatusEnum.valueOf(status);
-
-                    } catch (IllegalArgumentException ex) {
-                        statusEnum = Reparation.StatusEnum.NEW;
+                    switch(status) {
+                        case "Nieuw":
+                            statusEnum = Reparation.StatusEnum.NEW;
+                            break;
+                        case "Geaccepteerd":
+                            statusEnum = Reparation.StatusEnum.ACCEPTED;
+                            break;
+                        case "Toegekend":
+                            statusEnum = Reparation.StatusEnum.ASSIGNED;
+                            break;
+                        case "Gerepareerd":
+                            statusEnum = Reparation.StatusEnum.DONE;
+                            break;
+                        case "Afgemeld":
+                            statusEnum = Reparation.StatusEnum.REPAIRED;
+                            break;
                     }
 
                     Reparation reparation = new Reparation(nodeId, buildingEnum, floor1 ,location, latLng1, statusEnum, Reparation.PriorityType.values()[Integer.valueOf(Integer.valueOf(priority) - 1)], title, description, comments);
@@ -102,6 +113,7 @@ public class RepairReader {
 
         for(Reparation r : t) {
             String statusName = "Nieuw";
+            String PrioName = "Normaal";
 
             switch(r.Status) {
                 case NEW:
@@ -121,11 +133,32 @@ public class RepairReader {
                     break;
             }
 
+            switch(r.Priority) {
+                case URGENT:
+                    PrioName = "Urgent";
+                    break;
+                case IMPORTANT:
+                    PrioName = "Erg belangrijk";
+                    break;
+                case HIGH:
+                    PrioName = "Belangrijk";
+                    break;
+                case AVERAGE:
+                    PrioName = "Normaal";
+                    break;
+                case LOW:
+                    PrioName = "Laag";
+                    break;
+                case VERYLOW:
+                    PrioName = "Erg laag";
+                    break;
+            }
+
             List<String> subList = new ArrayList<String>();
             listDataHeader.add(r.ShortDescription);
             subList.add("Location:       " + r.Building + "" + r.Floor + "." + r.Location);
-            subList.add("Priority:          " + r.Priority);
-            subList.add("Status:           " + r.Status);
+            subList.add("Priority:          " + PrioName);
+            subList.add("Status:           " + statusName);
             subList.add("Description:  " + r.Description);
             subList.add("Comment:  " + r.Comment);
             listDataChild.put(r.ShortDescription, subList);
@@ -162,11 +195,53 @@ public class RepairReader {
                     String room = nodeRoom.getLocation();
 
 
+                    String statusName = "Nieuw";
+                    String PrioName = "Normaal";
+
+                    switch(status) {
+                        case "NEW":
+                            statusName = "Nieuw";
+                            break;
+                        case "ACCEPTED":
+                            statusName = "Geaccepteerd";
+                            break;
+                        case "ASSIGNED":
+                            statusName = "Toegekend";
+                            break;
+                        case "DONE":
+                            statusName = "Gerepareerd";
+                            break;
+                        case "REPAIRED":
+                            statusName = "Afgemeld";
+                            break;
+                    }
+
+                    switch(priority) {
+                        case "6":
+                            PrioName = "Urgent";
+                            break;
+                        case "5":
+                            PrioName = "Erg belangrijk";
+                            break;
+                        case "4":
+                            PrioName = "Belangrijk";
+                            break;
+                        case "3":
+                            PrioName = "Normaal";
+                            break;
+                        case "2":
+                            PrioName = "Laag";
+                            break;
+                        case "1":
+                            PrioName = "Erg laag";
+                            break;
+                    }
+
                     List<String> subList = new ArrayList<String>();
                     listDataHeader.add(title);
                     if(room != null) subList.add("Location:       " + room);
                     else subList.add("Location:       " + "C" + floor + "." + "16");
-                    subList.add("Priority:          " + Reparation.PriorityType.values()[Integer.valueOf(Integer.valueOf(priority) - 1)]);
+                    subList.add("Priority:          " + PrioName + "|" +Reparation.PriorityType.values()[Integer.valueOf(Integer.valueOf(priority) - 1)]);
                     subList.add("Status:           " + status);
                     subList.add("Description:  " + description);
                     subList.add("Comment:  " + comments);
