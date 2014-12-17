5b46cb75671417b913b292fc74a17ecaec07c272
DaveyHeeren <davey@daveyheeren.nl>
2014-12-17 11:31:24 +0100

    Push vibration

---
+++ MovinIndoor/app/src/main/java/project/movinindoor/Fragment/DFragment.java
@@ -0,0 +1,21 @@
+package project.movinindoor.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+/**
+ * Created by Davey on 17-12-2014.
+ */
+public class DFragment extends DialogFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.dialogfragment, container,
+                false);
+        getDialog().setTitle("DialogFragment Tutorial");
+        // Do something else
+        return rootView;
+    }
+}
