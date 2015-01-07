091d21c53e29c1ae1235833bc747e5aa0c34017b
Mike <mikevanloe@gmail.com>
2015-01-07 14:13:01 +0100

    fixed strategy

--- MovinIndoor/app/src/main/java/project/movinindoor/MovingBehaviour/MovingBehaviour.java
+++ MovinIndoor/app/src/main/java/project/movinindoor/MovingBehaviour/MovingBehaviour.java
@@ -3,8 +3,15 @@ package project.movinindoor.MovingBehaviour;
 /**
  * Created by 5736z454 on 7-1-2015.
  */
-public interface MovingBehaviour {
+public abstract class MovingBehaviour {
+    static int WALKING_SPEED;
     final static int MINUTE_IN_SEC = 3600;
 
-    public String calculateMovingSpeed(double cost);
+    public String calculateMovingSpeed(double cost){
+        float walkingspeedPerSecond = ((float) WALKING_SPEED) / MINUTE_IN_SEC;
+        double time = (cost / walkingspeedPerSecond);
+        int minute = (int) time / 60;
+        int second = (int) time % 60;
+        return String.format("%dm%02ds", minute, second);
+    }
 }
--- MovinIndoor/app/src/main/java/project/movinindoor/MovingBehaviour/moveByCart.java
+++ MovinIndoor/app/src/main/java/project/movinindoor/MovingBehaviour/moveByCart.java
@@ -3,17 +3,8 @@ package project.movinindoor.MovingBehaviour;
 /**
  * Created by 5736z454 on 7-1-2015.
  */
-public class moveByCart implements MovingBehaviour {
-    final static int WALKING_SPEED = 4000;
-
-    public moveByCart(){}
-
-    @Override
-    public String calculateMovingSpeed(double cost) {
-        float walkingspeedPerSecond = ((float) WALKING_SPEED) / MINUTE_IN_SEC;
-        double time = (cost / walkingspeedPerSecond);
-        int minute = (int) time / 60;
-        int second = (int) time % 60;
-        return String.format("%dm%02ds", minute, second);
+public class moveByCart extends MovingBehaviour {
+    public moveByCart(){
+        super.WALKING_SPEED = 4000;
     }
 }
